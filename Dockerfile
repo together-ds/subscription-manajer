e84ffcf3f68da4bbb7d36c3860579c005ea8edf3
together.ds <together.ds@gmail.com>
2023-07-16 15:50:06 +0800

    update github CI

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,9 @@ FROM azul/zulu-openjdk-alpine:17-jre
 VOLUME [/tmp , /config]
 ARG JAR_FILE=manajer-api/target/*.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["sh", "-c", "java ${JAVA_OPTS} -jar /app.jar ${0} ${@}", "--spring.config.name=application", "--spring.config.location=classpath:/application.yaml,file:/config/manajer.yaml"]
+ENTRYPOINT ["sh", \
+ "-c", \
+ "java ${JAVA_OPTS} -jar /app.jar ${0} ${@}", \
+ "--spring.config.name=application", \
+ "--spring.config.location=classpath:/application.yaml,file:/config/manajer.yaml" \
+ ]
--- manajer-ui/src/components/HelloWorld.vue
+++ manajer-ui/src/components/HelloWorld.vue
@@ -1,18 +1,6 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img
-        contain
-        height="300"
-        src="@/assets/logo.svg"
-      />
-
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
-
-      <div class="py-14" />
-
       <v-row class="d-flex align-center justify-center">
         <v-col cols="auto">
           <v-btn
@@ -22,18 +10,13 @@
             target="_blank"
             variant="text"
           >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
+            <v-icon icon="mdi-view-dashboard" size="large" start />
 
             Components
           </v-btn>
         </v-col>
 
         <v-col cols="auto">
-          {{axios}}
           <v-btn
             color="primary"
             href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
@@ -43,11 +26,7 @@
             target="_blank"
             variant="flat"
           >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
+            <v-icon icon="mdi-speedometer" size="large" start />
 
             Get Started
           </v-btn>
@@ -61,15 +40,45 @@
             target="_blank"
             variant="text"
           >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
+            <v-icon icon="mdi-account-group" size="large" start />
 
             Community
           </v-btn>
         </v-col>
+      </v-row>
+      <v-row v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+          <v-btn @click="decrement">-</v-btn>
+        </v-col>
+
+        <v-col cols="auto">
+          {{ count }}
+        </v-col>
+
+        <v-col cols="auto">
+          <v-btn @click="increment">+</v-btn>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn @click="createRandomBooks">create</v-btn>
+        </v-col>
+      </v-row>
+      <v-row v-row class="d-flex align-center justify-center">
+        <v-table>
+          <thead>
+            <tr>
+              <th class="text-left">Id</th>
+              <th class="text-left">Title</th>
+              <th class="text-left">Author</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in books" :key="item.name">
+              <td>{{ item.id }}</td>
+              <td>{{ item.title }}</td>
+              <td>{{ item.author }}</td>
+            </tr>
+          </tbody>
+        </v-table>
       </v-row>
     </v-responsive>
   </v-container>
@@ -77,7 +86,7 @@
 
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   // data() 返回的属性将会成为响应式的状态
@@ -85,24 +94,52 @@ export default {
   data() {
     return {
       count: 0,
-      axios: 'axios...'
-    }
+      books: [],
+    };
   },
 
   // methods 是一些用来更改状态与触发更新的函数
   // 它们可以在模板中作为事件监听器绑定
   methods: {
     increment() {
-      this.count++
-    }
+      // log count and ++
+      console.log(this.count);
+      this.count++;
+      console.log(this.count);
+    },
+    decrement() {
+      // log count and --
+      console.log(this.count);
+      this.count--;
+      console.log(this.count);
+    },
+    createRandomBooks() {
+      this.books = [];
+      for (var i = 0; i < this.count; i++) {
+        //随机书名和作者，出版时间
+        this.books.push({
+          id: i + 1,
+          title: this.randomName(6),
+          author: this.randomName(10),
+          published: this.randomDate(),
+        });
+      }
+    },
+    randomDate() {// generate a random date
+      return new Date(Date.now() + Math.random() * 1000 * 60 * 60 * 24);
+    },
+    randomName(length){
+      return Math.random().toString(36).substr(2, length);
+    },
+    
   },
 
   // 生命周期钩子会在组件生命周期的各个不同阶段被调用
   // 例如这个函数就会在组件挂载完成后被调用
   async mounted() {
-    console.log(`The initial count is ${this.count}.`)
-    let resp = await axios.get('/subscribe/get-template')
+    console.log(`The initial count is ${this.count}.`);
+    let resp = await axios.get("/subscribe/get-template");
     this.axios = resp;
-  }
-}
+  },
+};
 </script>
