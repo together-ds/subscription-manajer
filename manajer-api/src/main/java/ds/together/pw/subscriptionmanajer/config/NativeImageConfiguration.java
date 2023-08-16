53eb0ca83b8d77d3815c83d32e29bf251c11a664
together.ds <together.ds@gmail.com>
2023-08-17 03:30:21 +0800

    add migration sql

--- manajer-api/src/main/java/ds/together/pw/subscriptionmanajer/config/NativeImageConfiguration.java
+++ manajer-api/src/main/java/ds/together/pw/subscriptionmanajer/config/NativeImageConfiguration.java
@@ -3,8 +3,11 @@ package ds.together.pw.subscriptionmanajer.config;
 import ds.together.pw.subscriptionmanajer.entity.ProxyGroup;
 import ds.together.pw.subscriptionmanajer.entity.Subscription;
 import ds.together.pw.subscriptionmanajer.entity.SubscriptionResult;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 /**
  * 需要序列化的类，放到RegisterReflectionForBinding里。否则 native image无法完成序列化
@@ -19,5 +22,12 @@ import org.springframework.context.annotation.Configuration;
 		Subscription.class,
 		SubscriptionResult.class
 })
-public class RegisterReflectionConfiguration {
+@ImportRuntimeHints(MigrationRuntimeHints.class)
+public class NativeImageConfiguration {
+}
+class MigrationRuntimeHints implements RuntimeHintsRegistrar {
+	@Override
+	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+		hints.resources().registerPattern("db/migration/*.sql");
+	}
 }
